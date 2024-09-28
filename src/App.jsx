66508e3156c124838600016d40a6866872ef247b
Niko Niemelä <nokka200@gmail.com>
2024-09-28 11:46:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes } from './services/requests'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1
+  })
+
+  const anecdotes = result.data || []
+
+  if (result.isPending) {
+    return <div>loading...</div>
+  }
+
+  if (result.isError) {
+    console.log(result.error)
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
